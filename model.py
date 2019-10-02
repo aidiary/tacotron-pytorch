3e979a92dcf9d531a1837f7406448f4974a7beaf
Koichiro Mori <f2forest@gmail.com>
2019-10-02 15:29:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class Tacotron(nn.Module):
 
-    def __init__(self, num_chars, r=5, linear_dim=1025, mel_dim=80):
+    def __init__(self, num_chars, r=7, linear_dim=1025, mel_dim=80):
         super(Tacotron, self).__init__()
         self.r = r
         self.linear_dim = linear_dim
@@ -37,7 +37,173 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    pass
+
+    def __init__(self, in_features, memory_dim, r=7):
+        super(Decoder, self).__init__()
+
+        # Encoderの出力の次元数
+        self.in_features = in_features
+        # 1つ前の時刻のDecoderの出力の次元数（rフレーム分）
+        self.memory_dim = memory_dim
+        # decodeの各ステップでrフレーム分をまとめて出力する
+        self.memory_size = r
+        # rフレーム分をまとめて処理するが訓練が進むに連れて短くなる
+        self.r_init = r
+        self.r = r
+        self.max_decoder_steps = 500
+        self.query_dim = 256
+
+        self.prenet = Prenet(
+            self.memory_dim,
+            out_features=[256, 128])
+
+        self.attention_rnn = nn.GRUCell(in_features + 128, self.query_dim)
+
+        self.attention = Attention(query_dim=self.query_dim,
+                                   embedding_dim=in_features,
+                                   attention_dim=128)
+
+        # (processed_memory | attention context) -> |Linear| -> decoder RNN input
+        self.project_to_decoder_in = nn.Linear(256 + in_features, 256)
+        # decoder RNN input -> |RNN| -> RNN state
+        self.decoder_rnns = nn.ModuleList(
+            [nn.GRUCell(256, 256) for _ in range(2)]
+        )
+        # RNN state -> |Linear| -> mel_spec
+        self.proj_to_mel = nn.Linear(256, self.memory_dim * self.r_init)
+        self.stopnet = StopNet(256 + memory_dim * self.r_init)
+
+    def forward(self, inputs, memory, mask):
+        """
+        Args:
+            inputs: Encoder outputs.
+            memory: Decoder memory (autoregression. If None (at eval-time),
+              decoder outputs are used as decoder inputs. If None, it uses the last
+              output as the input.
+            mask: Attention mask for sequence padding.
+
+        Shapes:
+            - inputs: batch x time x encoder_out_dim
+            - memory: batch x #mel_specs x mel_spec_dim
+        """
+        # 入力となるメルスペクトログラム系列をrフレームごとにまとめて特徴量とみなす変換
+        memory = self._reshape_memory(memory)
+
+        outputs = []
+        attentions = []
+        stop_tokens = []
+        t = 0
+        self._init_states(inputs)
+        self.attention.init_states(inputs)
+        # 訓練なので教師のメルスペクトログラムの系列長まで生成
+        while len(outputs) < memory.size(0):
+            if t > 0:
+                # 0フレーム目は0初期化
+                # 1つ前のフレームのメルスペクトログラムが入る
+                new_memory = memory[t - 1]
+                self._update_memory_input(new_memory)
+            output, stop_token, attention = self.decode(inputs, mask)
+            outputs += [output]
+            attentions += [attention]
+            stop_tokens += [stop_token]
+            t += 1
+
+        return self._parse_outputs(outputs, attentions, stop_tokens)
+
+    def decode(self, inputs, mask=None):
+        """
+        inputsはEncoderの出力
+
+        decode()は2つのRNNが連結してできている
+        decode()は1ステップ分の処理なのでGRUCellでできている
+        1. Attention RNN => attention_rnn_hidden
+        2. Decoder RNN   => decoder_rnn_hiddens[]
+        それぞれ内部状態を持ち各ステップの間で保持している
+        Decoder RNNは2層GRUになっている
+        """
+        # Prenet
+        processed_memory = self.prenet(self.memory_input)
+
+        # Attention RNN
+        # hiddenが循環していることに注意！
+        # 次のstepでは更新されたhiddenが使われる
+        self.attention_rnn_hidden = self.attention_rnn(
+            torch.cat((processed_memory, self.context_vec), -1),
+            self.attention_rnn_hidden)
+
+        # Attention
+        # AttentionはAttention RNNの隠れ状態とEncoder出力の間で行われる
+        # processed_inputsはinputsをLinearに通してサイズ変更したtensor
+        # TODO: processed_inputsは外から渡さなくてもattentionの中で計算できそう
+        self.context_vec = self.attention(
+            self.attention_rnn_hidden, inputs, self.processed_inputs, mask)
+
+        # Decoder RNN
+        # Attention RNNのhiddenとcontext vectorを入力する
+        decoder_input = self.project_to_decoder_in(
+            torch.cat((self.attention_rnn_hidden, self.context_vec), -1))
+
+        # Decoder RNNは複数のGRU層からなる
+        # 隠層で異なる隠れ状態をもつ
+        for idx in range(len(self.decoder_rnns)):
+            self.decoder_rnn_hiddens[idx] = self.decoder_rnns[idx](
+                decoder_input, self.decoder_rnn_hiddens[idx])
+            # 次の層のRNNへは入力を足しこむ
+            # Residual connection
+            decode_input = self.decoder_rnn_hiddens[idx] + decoder_input
+        decoder_output = decoder_input
+
+        # Decoderの出力からメルスペクトログラムを予測
+        output = self.proj_to_mel(decoder_output)
+
+        # Stop tokenを予測
+        # このステップで停止するかを予測するネットワーク
+        stopnet_input = torch.cat([decoder_output, output], -1)
+        stop_token = self.stopnet(stopnet_input.detach())
+        output = output[:, :self.r * self.memory_dim]
+        return output, stop_token, self.attention.attention_weights
+
+    def _reshape_memory(self, memory):
+        """
+        Reshape the spectrograms for given 'r'
+        """
+        B = memory.shape[0]
+        # rフレームずつをまとめて特徴量とみなす
+        if memory.size(-1) == self.memory_dim:
+            memory = memory.contiguous()
+            memory = memory.view(B, memory.size(1) // self.r, -1)
+        # (seq_len, batch, input_size) に変換
+        memory = memory.transpose(0, 1)
+        return memory
+
+    def _update_memory_input(self, new_memory):
+        self.memory_input = new_memory[:, :self.memory_dim]
+
+    def _init_states(self, inputs):
+        """
+        Initialize decoder states
+        """
+        B = inputs.size(0)
+        T = inputs.size(1)
+
+        self.memory_input = torch.zeros(
+            B, self.memory_dim, device=inputs.device)
+        self.attention_rnn_hidden = torch.zeros(B, 256, device=inputs.device)
+        self.decoder_rnn_hiddens = [
+            torch.zeros(B, 256, device=inputs.device) for _ in range(len(self.decoder_rnns))
+        ]
+        self.context_vec = inputs.data.new(B, self.in_features).zero_()
+        self.processed_inputs = self.attention.inputs_layer(inputs)
+
+    def _parse_outputs(self, outputs, attentions, stop_tokens):
+        # outputsなどは (batch, input_size) で長さが時間長のリストになっている
+        # 時間方向にstackした後に (batch, seq_len, input_size) に戻す
+        attentions = torch.stack(attentions).transpose(0, 1)
+        outputs = torch.stack(outputs).transpose(0, 1).contiguous()
+        stop_tokens = torch.stack(stop_tokens).transpose(0, 1).squeeze(-1)
+        return outputs, attentions, stop_tokens
+
+    # TODO: inferenceを追加
 
 
 class CBHG(nn.Module):
@@ -47,10 +213,10 @@ class CBHG(nn.Module):
         - Bidirectional gated recurrent units
 
         Args:
-            in_features (int): sample size
-            K (int): max filter size in conv bank
-            projections (list): conv channel sizes for conv projections
-            num_highways (int): number of highways layers
+            in_features(int): sample size
+            K(int): max filter size in conv bank
+            projections(list): conv channel sizes for conv projections
+            num_highways(int): number of highways layers
 
         Shapes:
             - input: batch x time x dim
@@ -217,7 +383,7 @@ class Prenet(nn.Module):
 
     def forward(self, x):
         for linear in self.layers:
-            x = F.dropout(F.relu(linear(x)), p=0.5, training=self.training)
+            x = F.dropout(F.relu(linear(x)), p=0.5)
         return x
 
 
@@ -236,3 +402,94 @@ class Highway(nn.Module):
         H = self.relu(self.H(inputs))
         T = self.sigmoid(self.T(inputs))
         return H * T + inputs * (1.0 - T)
+
+
+class Attention(nn.Module):
+
+    def __init__(self, query_dim, embedding_dim, attention_dim):
+        super(Attention, self).__init__()
+        self.query_layer = Linear(
+            query_dim, attention_dim, bias=False, init_gain='tanh')
+        self.inputs_layer = Linear(
+            embedding_dim, attention_dim, bias=False, init_gain='tanh')
+        self.v = Linear(attention_dim, 1, bias=True)
+        self.location_layer = LocationLayer(attention_dim)
+        self._mask_value = -float('inf')
+
+    def forward(self, query, inputs, processed_inputs, mask):
+        # Location-sensitive attention
+        attention, _ = self.get_location_attention(query, processed_inputs)
+        if mask is not None:
+            attention.data.masked_fill_(~mask, self._mask_value)
+        alignment = torch.sigmoid(attention) / \
+            torch.sigmoid(attention).sum(dim=1, keepdim=True)
+        self.update_location_attention(alignment)
+
+        # context vector
+        context = torch.bmm(alignment.unsqueeze(1), inputs)
+        context = context.squeeze(1)
+
+        # current stepのattention重みをインスタンスとして保持しておく
+        # location-sensitive
+        self.attention_weights = alignment
+
+        return context
+
+    def init_states(self, inputs):
+        B = inputs.shape[0]
+        T = inputs.shape[1]
+        self.attention_weights = torch.zeros([B, T], dtype=torch.float32)
+        self.attention_weights_cum = torch.zeros([B, T], dtype=torch.float32)
+
+    def get_location_attention(self, query, processed_inputs):
+        attention_cat = torch.cat((self.attention_weights.unsqueeze(1),
+                                   self.attention_weights_cum.unsqueeze(1)), dim=1)
+        processed_query = self.query_layer(query.unsqueeze(1))
+        processed_attention_weights = self.location_layer(attention_cat)
+        energies = self.v(
+            torch.tanh(processed_query +
+                       processed_attention_weights + processed_inputs))
+        energies = energies.squeeze(-1)
+        return energies, processed_query
+
+    def update_location_attention(self, alignment):
+        self.attention_weights_cum += alignment
+
+
+class LocationLayer(nn.Module):
+    def __init__(self,
+                 attention_dim,
+                 attention_n_filters=32,
+                 attention_kernel_size=31):
+        super(LocationLayer, self).__init__()
+        self.location_conv = nn.Conv1d(
+            in_channels=2,
+            out_channels=attention_n_filters,
+            kernel_size=attention_kernel_size,
+            stride=1,
+            padding=(attention_kernel_size - 1) // 2,
+            bias=False)
+        self.location_dense = Linear(
+            attention_n_filters, attention_dim, bias=False, init_gain='tanh')
+
+    def forward(self, attention_cat):
+        processed_attention = self.location_conv(attention_cat)
+        processed_attention = self.location_dense(
+            processed_attention.transpose(1, 2))
+        return processed_attention
+
+
+class StopNet(nn.Module):
+
+    def __init__(self, in_features):
+        super(StopNet, self).__init__()
+        self.dropout = nn.Dropout(0.1)
+        self.linear = nn.Linear(in_features, 1)
+        torch.nn.init.xavier_uniform_(
+            self.linear.weight, gain=torch.nn.init.calculate_gain('linear'))
+
+    def forward(self, inputs):
+        # TODO: dropoutが先であっている？
+        outputs = self.dropout(inputs)
+        outputs = self.linear(outputs)
+        return outputs
