0fcd57adb5336d07c39ce39ce617e1e2f5ffb5c5
Koichiro Mori <f2forest@gmail.com>
2019-10-04 13:54:51 +0900

    Fix a bug

--- loss.py
+++ loss.py
@@ -8,7 +8,7 @@ class L1LossMasked(nn.Module):
     def forward(self, x, target, length):
         target.requires_grad = False
         mask = sequence_mask(sequence_length=length,
-                             maxlen=target.size(1).unsqueeze(2).float())
+                             maxlen=target.size(1)).unsqueeze(2).float()
         mask = mask.expand_as(x)
         loss = F.l1_loss(x * mask, target * mask, reduction='sum')
         loss = loss / mask.sum()
